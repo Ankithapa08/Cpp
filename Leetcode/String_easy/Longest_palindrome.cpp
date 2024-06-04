61886fc66d751238f09fb77a1e5cbe53c22e2c1a
Ankit Thapa <145375362+Ankithapa08@users.noreply.github.com>
2024-06-04 23:30:25 +0530

    Create Longest_palindrome.cpp

---
+++ Leetcode/String_easy/Longest_palindrome.cpp
@@ -0,0 +1,21 @@
+//Leetcode Problem->409
+  int longestPalindrome(string s) {
+    int n=s.size(),count=0;
+vector<int> v(52);
+for(int i=0;i<n;i++){
+if((int(s[i]))>96){
+   v[(int(s[i])-'a')]++;
+}
+else{
+  v[(int(s[i])-'A'+26)]++;
+}
+}
+for(int i=0;i<52;i++){
+if(v[i]%2==1){
+count++;
+}
+if(count>0){
+  count--;
+}
+return s.size()-1;
+}
